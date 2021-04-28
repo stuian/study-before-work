b6eb07f8fd7b49dd4a934906a405f4057d44e701
stuian <892545949@qq.com>
2021-04-28 22:54:16 +0800

    210428

---
+++ C_and_C++/C_exercise/file.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+    FILE *fp1;
+    FILE *fp2;
+    int ch;
+
+    if((fp1=fopen("hello.txt","r"))== NULL)
+    {
+        printf("文件打开失败!\n");
+        exit(EXIT_FAILURE);
+    }
+    if((fp2=fopen("world.txt","w"))== NULL)
+    {
+        printf("文件打开失败!\n");
+        exit(EXIT_FAILURE);
+    }
+    while((ch=fgetc(fp1))!= EOF)
+    {
+        fputc(ch,fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
